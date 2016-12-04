782d6685b7fa0f22f1a59992dda4619164c30d30
Arturo Salinas <arturo.salinas.g@usach.cl>
2016-12-03 21:32:08 -0300

    Ahora si, TestController y templates/test/index.ctp

---
+++ src/Controller/TestController.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\Event\Event;
+
+/**
+ * Admins Controller
+ *
+ * @property \App\Model\Table\AdminsTable $Admins
+ */
+class TestController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Network\Response|null
+     */
+    public function index()
+    {
+       //Desde acá teni que poner $this->loadModel('Nombre del modelo'); para cargar el modelo
+       //Luego, puedes llamar las weas del modelo con $this->NombreDelModelo->funcion(); o cosas asi
+    	//Ej:
+    	//$this->loadModel('Volunteers');
+    	//$this->Volunteers->saveNewVolunteer($this->request->data, $user->id); (la funcion que hice en la wea de voluntarios)
+    	//Obviamente tení que hacer la funcion en la clase del modelo correspondiente (Para el caso anterior, era VolunteersTable)
+    	//esa cosa está en src/Table, ahi pillas las clases Table para meter todo.
+
+    	//Para ejecutar toda esta mierda, llama a localhost o donde sea/test/index
+    	//poi
+
+    	//No rompas nada :^)
+
+    } 
+
+    public function beforeFilter(Event $event)
+    {
+        $this->Auth->allow(['index']);
+
+    }
+
+
+    
+}
---
+++ src/Template/Test/index.ctp
@@ -0,0 +1,5 @@
+<div>
+<h1>Backend test site</h1>
+<h2>Get away!</h2>
+
+</div>
