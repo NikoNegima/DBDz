013b0fec26cbef4bacfedb598e56e54e173a344b
NikoNegima <nicolas.roman@usach.cl>
2016-12-09 01:11:29 -0300

    Conexion layout -> Managers :dog:

--- config/app.php
+++ config/app.php
@@ -231,8 +231,6 @@ return [
             //'port' => 'non_standard_port_number',
             'username' => 'root',
             'password' => '',
-            'database' => 'DBD_Emergency',
-            'password' => '',
             'database' => 'DBDz',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
